2a4d9e46ea0510eeb7aa893e169d035718d6e98c
dmohle <10038913+dmohle@users.noreply.github.com>
2024-10-03 12:19:15 -0700

    Add files via upload

--- Animal.java
+++ Animal.java
@@ -1,4 +1,5 @@
 package dennis.zoo.com;
+
 import java.util.Date;
 
 public class Animal {
@@ -10,38 +11,78 @@ public class Animal {
         numOfAnimals++;
     }
 
-    // Create a constructor that accepts a name
-    public Animal(String aName) {
-        this.animalName = aName;
-        numOfAnimals++;
+    // Create a constructor that will accept all fields as arguments.
+    public Animal(String sex, int age, int weight, String animalName,
+                  String animalID, String animalBirthDate, String animalColor,
+                  String animalOrigin) {
+        this.sex = sex;
+        this.age = age;
+        this.weight = weight;
+        this.animalName = animalName;
+        this.animalID = animalID;
+        this.animalBirthDate = animalBirthDate;
+        this.animalColor = animalColor;
+        this.animalOrigin = animalOrigin;
     }
 
 
-    // Create all attributes (fields) that we need
+    // Create all attributes (fields) needed for our midterm program.
     // sex will be 'male' or 'female'
     private String sex;
     // age will be in years
     private int age;
     // weight will be in pounds
     private int weight;
-    // a unique ID for each animal
-    private String animalID;
-    // animal names come from a text file called animalNames.txt
+    // name will be a String;
     private String animalName;
-    // animal birthdate.
-    private Date animalBirthdate;
-    // animal color
+    // animalID
+    private String animalID;
+    // animalBirthDate is going to be a string here (in this class)
+    private String animalBirthDate;
+    // animalColor is a String
     private String animalColor;
-    // origin zoo
+    // origin will be a string like: "from Friguia Park, Tunisia"
     private String animalOrigin;
-    // arrival date
-    private Date animalArrivalDate;
-
 
 
 
 
     // Create getters and setters
+
+    public String getAnimalOrigin() {
+        return animalOrigin;
+    }
+    public void setAnimalOrigin(String animalOrigin) {
+        this.animalOrigin = animalOrigin;
+    }
+
+    public String getAnimalColor() {
+        return animalColor;
+    }
+    public void setAnimalColor(String animalColor) {
+        this.animalColor = animalColor;
+    }
+
+    public String getAnimalBirthDate() {
+        return animalBirthDate;
+    }
+    public void setAnimalBirthDate(String animalBirthDate) {
+        this.animalBirthDate = animalBirthDate;
+    }
+
+    public String getAnimalID() {
+        return animalID;
+    }
+    public void setAnimalID(String animalID) {
+        this.animalID = animalID;
+    }
+
+    public String getAnimalName() {
+        return animalName;
+    }
+    public void setAnimalName(String animalName) {
+        this.animalName = animalName;
+    }
 
     public int getAge() {
         return age;
@@ -60,32 +101,8 @@ public class Animal {
     public int getWeight() {
         return weight;
     }
+
     public void setWeight(int weight) {
         this.weight = weight;
     }
-
-    public String getAnimalID() {return animalID;}
-    public void setAnimalID(String animalID) { this.animalID = animalID;}
-
-    public String getAnimalName() {return animalName;}
-    public void setAnimalName(String animalName) { this.animalName = animalName;}
-
-    public Date getAnimalBirthdate() {return animalBirthdate;}
-    public void setAnimalBirthdate(Date animalBirthdate) {this.animalBirthdate = animalBirthdate;}
-
-    public String getAnimalColor() {return animalColor;}
-    public void setAnimalColor(String animalColor) {this.animalColor = animalColor;}
-
-    public String getAnimalOrigin() {return animalOrigin;}
-    public void setAnimalOrigin(String animalOrigin) {this.animalOrigin = animalOrigin;}
-
-    public Date getAnimalArrivalDate() {return animalArrivalDate;}
-    public void setAnimalArrivalDate(Date animalArrivalDate) {this.animalArrivalDate = animalArrivalDate;}
-
-
-
-
-
-
-
 }
